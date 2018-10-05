b035fd9e01ef495463efa34969ffbda612dd08af
mplescano <mplescano@>
2018-10-05 00:01:10 -0500

    Add script for z2b webapp

---
+++ fabric-ca-zero/scripts/sdks-install-zerotoblockchain-webapp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+mkdir $HOME/developer || true
+cd $HOME/developer || true
+rm -Rf $HOME/developer/zerotoblockchain-webapp || true
+cp -rf /stuff/zerotoblockchain-webapp $HOME/developer
+cp -f /stuff/node_modules.tar.gz $HOME/developer/zerotoblockchain-webapp
+cd $HOME/developer/zerotoblockchain-webapp
+gunzip < node_modules.tar.gz | tar -xv
+rm -f node_modules.tar.gz
+
+npm install
+
+if [[ ! -z "${http_proxy}" ]]; then
+   temp_http_proxy=$http_proxy
+   temp_https_proxy=$https_proxy
+   temp_no_proxy=$no_proxy
+   unset http_proxy
+   unset https_proxy
+   unset no_proxy
+fi
+
+node index
